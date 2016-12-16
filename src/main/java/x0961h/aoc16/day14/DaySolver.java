93d7133198f7b175a32c642ff83886d4e8615dfd
0x0961h <0x0961h@gmail.com>
2016-12-16 18:25:34 +0300

    Day 14 Solution Speed Improvement

--- src/main/java/x0961h/aoc16/day14/DaySolver.java
+++ src/main/java/x0961h/aoc16/day14/DaySolver.java
@@ -3,26 +3,34 @@ package x0961h.aoc16.day14;
 import x0961h.aoc16.utils.MD5;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by 0x0961h on 16.12.16.
  */
 public class DaySolver {
+    private static final String SALT = "qzyelonm";
+    private static Map<Integer, String> hashCache = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
-        System.out.println("Result = " + solve("qzyelonm"));
+        long time = new Date().getTime();
+        System.out.println("Result = " + solve());
+        System.out.println("Time elapsed: " + (new Date().getTime() - time) + "ms");
     }
 
-    public static int solve(String salt) {
+    public static int solve() {
         int keysGenerated = 0;
         int index = 0;
         while (keysGenerated < 64) {
-            String hash = MD5.get(salt + index);
+            String hash = getHash(index);
 
             for (int i = 0; i < hash.length() - 2; i++) {
                 if (hash.charAt(i) == hash.charAt(i + 1) && hash.charAt(i) == hash.charAt(i + 2)) {
                     cycle2:
                     for (int index2 = index + 1; index2 <= index + 1000; index2++) {
-                        String hash2 = MD5.get(salt + index2);
+                        String hash2 = getHash(index2);
 
                         for (int j = 0; j < hash2.length() - 4; j++) {
                             if (hash2.charAt(j) == hash.charAt(i) &&
@@ -46,5 +54,10 @@ public class DaySolver {
         }
 
         return index - 1;
+    }
+
+    private static String getHash(int index) {
+        if (!hashCache.containsKey(index)) hashCache.put(index, MD5.get(SALT + index));
+        return hashCache.get(index);
     }
 }
